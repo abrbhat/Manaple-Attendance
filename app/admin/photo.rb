1d44ff781fa0961fc03e9dcae068a9fd3e84bd37
peeyush <peeyushagarwal1994@gmail.com>
2014-05-21 21:42:41 +0530

    Activeadmin working properly

---
+++ app/admin/photo.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Photo do
+
+  permit_params :description, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
