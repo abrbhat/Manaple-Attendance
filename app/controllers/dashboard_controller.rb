4dd3a44226bc136874e374d9f9ed83600bdd7665
abrbhat <bhatnagarabhiroop@gmail.com>
2014-05-27 02:10:55 +0530

    Before trying reset

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class DashboardController < ApplicationController
+  before_filter :authenticate_user!
   def index
+  end
+  def notifications
+  end
+  def employees
+  end
+  def attendance_today
+  	@stores = current_user.stores
+  	@stores.each do |store|
+  		
+  	end
   end
 end
