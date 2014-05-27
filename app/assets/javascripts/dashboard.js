c1715bb98f557a49142721bfc441452e64356678
abrbhat <bhatnagarabhiroop@gmail.com>
2014-05-28 02:08:05 +0530

    time period count working fine

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 	$('#attendance-data-date-chooser').datepicker({
 					format: 'dd-mm-yyyy'
 				});
+	$('#attendance-data-time-period-start-chooser').datepicker({
+					format: 'dd-mm-yyyy'
+				});
+	$('#attendance-data-time-period-end-chooser').datepicker({
+					format: 'dd-mm-yyyy'
+				});
 });
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -53,5 +53,32 @@ class DashboardController < ApplicationController
     end
   end
   def attendance_time_period
+    @stores = current_user.stores
+    @attendance_data_today = []
+    @stores.each do |store|
+      store.employees.each do |employee|
+        attendance_data = Hash.new
+        attendance_data["present_count"] = 0
+        if params[:time_period_start].blank?
+          @start_date = Time.now
+          @end_date = Time.now
+          @date = Time.now.strftime("%d-%m-%Y")
+        else
+          @start_date = DateTime.parse(params[:time_period_start])
+          @end_date = DateTime.parse(params[:time_period_end])
+          @date = params[:date]
+        end
+        photos = employee.photos.where(created_at: (@start_date.midnight..@end_date.midnight + 1.day))
+        photos.each do |photo|
+          if photo.description == 'in'          
+            attendance_data["present_count"] = attendance_data["present_count"] + 1
+          end
+        end
+        @working_days = (@end_date-@start_date).to_i + 1
+        attendance_data["absent_count"] = @working_days - attendance_data["present_count"]
+        attendance_data["employee"] = employee
+        @attendance_data_today << attendance_data
+      end
+    end
   end
 end
