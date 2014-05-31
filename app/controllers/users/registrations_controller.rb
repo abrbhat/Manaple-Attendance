22894891b3c175e233ede5338c5efd8903ea4005
abrbhat <bhatnagarabhiroop@gmail.com>
2014-06-01 02:20:04 +0530

    nothing new

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 	protected
 	def sign_up(resource_name, resource)
-    	
+
   	end
+
   	def after_sign_up_path_for(resource)
     	sign_out :user
       	flash[:error] = resource.name+", "+resource.email+" has been created!"
       	new_user_session_path
   	end
+  	protected
+
 end
