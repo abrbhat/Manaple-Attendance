100763164702855f39f1717b5c17299327c11b24
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2015-03-17 15:37:04 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,13 +58,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # config.paperclip_defaults = {
-  #   storage: :s3,
-  #   s3_credentials: {
-  #     bucket: 'photos-webapp',
-  #     access_key_id: 'AKIAIZ5UVV37QUAPPSUQ',
-  #     secret_access_key: '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'
-  #   }
-  # }
 
 end
