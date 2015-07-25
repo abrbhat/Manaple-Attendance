935b4e95608d484c989410ddb80850e9f789026d
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2015-07-25 12:56:45 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 require "smtp_tls"
 
 ActionMailer::Base.smtp_settings = {
-:address => ENV["SMTP_ADDRESS"],,
+:address => ENV["SMTP_ADDRESS"],
 :port => 587,
 :authentication => :plain,
 :user_name => ENV["MAIL_USERNAME"],
